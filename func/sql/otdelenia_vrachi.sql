85c687db6f40674ffbe3c17180054a6035ea6d29
oleg_medovikov <oleg.medovikov@mail.ru>
2024-07-05 18:24:59 +0300

    update vrachi

--- func/sql/otdelenia_vrachi.sql
+++ func/sql/otdelenia_vrachi.sql
@@ -1,8 +1,11 @@
-SELECT distinct p.fio, c.nrPmuDepartName, c.fedPosition, c.postId,
-    CONVERT(VARCHAR, c.beginDate, 104) AS beginDate, 
-    CONVERT(VARCHAR, c.endDate, 104) AS endDate, 
-    s.specNames
-  from (select * FROM [FRMR2].[dbo].[Cards]   WHERE oid = '__OID__') as  c
+SELECT distinct p.fio, c.nrPmuDepartName, c.fedPosition,
+  '' as 'postId',
+  CONVERT(VARCHAR, c.beginDate, 104) AS beginDate, 
+  CONVERT(VARCHAR, c.endDate, 104) AS endDate, 
+  s.specNames, c.nrPmuDepartId, nsi.Id
+  from (select * FROM [FRMR2].[dbo].[Cards]   WHERE oid = '__OID__'
+		and (endDate is null or endDate > GETDATE())
+	) as  c
   inner join (SELECT 
       UPPER(LEFT(ISNULL(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE([lastName], '5', 'о'), '6', 'г'), '7', 'в'), '8', 'б'), '9', 'а'), ''), 1)) + 
       LOWER(SUBSTRING(ISNULL(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE([lastName], '5', 'о'), '6', 'г'), '7', 'в'), '8', 'б'), '9', 'а'), ''), 2, LEN(ISNULL([lastName], ''))))
@@ -20,8 +23,10 @@ SELECT distinct p.fio, c.nrPmuDepartName, c.fedPosition, c.postId,
 						WHERE innerCerts.mrOid = outerCerts.mrOid
 						FOR XML PATH(''), TYPE).value('.', 'NVARCHAR(MAX)'), 1, 2, '') AS specNames
 				FROM [FRMR2].[dbo].[Certs] AS outerCerts
-				--WHERE endDate <= getdate()  -- это чтобы отсеять устаревшую специализацию
- 				GROUP BY  mrOid
+				--WHERE endDate <= getdate() 
+				GROUP BY  mrOid
 				) as s
 		on (c.mrOid = s.mrOid)
+	left join (SELECT [Id],[DepartOid] FROM [NsiBase].[Regiz].[MedOrganizations]) nsi
+		on(c.nrPmuDepartId = nsi.DepartOid)
   order by c.nrPmuDepartName, c.fedPosition
