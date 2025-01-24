e16d8f99d45b6135868b689efa8b47a12e482e39
oleg_medovikov <oleg.medovikov@mail.ru>
2025-01-24 17:52:51 +0300

    исправил вкладку сотрудники

--- func/sql/sotrydniki.sql
+++ func/sql/sotrydniki.sql
@@ -1,29 +1,31 @@
-SELECT distinct p.lastName, p.firstName, p.patronymic,  p.birthDate, p.snils, c.nrPmuDepartName, c.fedPosition,
-  CONVERT(VARCHAR, c.beginDate, 104) AS beginDate, 
-  s.specNames, c.nrPmuDepartId, nsi.Id
-  from (select * FROM [FRMR2].[dbo].[Cards]   WHERE oid = '__OID__'
-		and (endDate is null or endDate > GETDATE())
-	) as  c
-  inner join (SELECT 
-      UPPER(LEFT(ISNULL(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE([lastName], '5', 'о'), '6', 'г'), '7', 'в'), '8', 'б'), '9', 'а'), ''), 1)) + 
-      LOWER(SUBSTRING(ISNULL(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE([lastName], '5', 'о'), '6', 'г'), '7', 'в'), '8', 'б'), '9', 'а'), ''), 2, LEN(ISNULL([lastName], '')))) as lastName,
-      UPPER(LEFT(ISNULL(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE([firstName], '5', 'о'), '6', 'г'), '7', 'в'), '8', 'б'), '9', 'а'), ''), 1)) + 
-      LOWER(SUBSTRING(ISNULL(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE([firstName], '5', 'о'), '6', 'г'), '7', 'в'), '8', 'б'), '9', 'а'), ''), 2, LEN(ISNULL([firstName], '')))) as firstName,
-      UPPER(LEFT(ISNULL(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE([patronymic], '5', 'о'), '6', 'г'), '7', 'в'), '8', 'б'), '9', 'а'), ''), 1)) + 
-      LOWER(SUBSTRING(ISNULL(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE([patronymic], '5', 'о'), '6', 'г'), '7', 'в'), '8', 'б'), '9', 'а'), ''), 2, LEN(ISNULL([patronymic], '')))) AS patronymic,
-	  cast(birthDate as date) birthDate, snils,oid
-		FROM [FRMR2].[dbo].[Persons]) as p
-		on (p.oid = c.mrOid)
-	left join(SELECT mrOid, 
-					STUFF((SELECT DISTINCT ', ' + specName FROM [FRMR2].[dbo].[Certs] AS innerCerts
-						WHERE innerCerts.mrOid = outerCerts.mrOid
-						FOR XML PATH(''), TYPE).value('.', 'NVARCHAR(MAX)'), 1, 2, '') AS specNames
-				FROM [FRMR2].[dbo].[Certs] AS outerCerts
-				--WHERE endDate <= getdate() 
-				GROUP BY  mrOid
-				) as s
-		on (c.mrOid = s.mrOid)
-	left join (SELECT [Id],[DepartOid] FROM [NsiBase].[Regiz].[MedOrganizations]) nsi
-		on(c.nrPmuDepartId = nsi.DepartOid)
-  order by c.nrPmuDepartName, c.fedPosition
-
+SELECT DISTINCT
+	p.oid,
+    UPPER(LEFT(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(p.lastName, '5', 'о'), '6', 'г'), '7', 'в'), '8', 'б'), '9', 'а'), 1)) 
+    + LOWER(SUBSTRING(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(p.lastName, '5', 'о'), '6', 'г'), '7', 'в'), '8', 'б'), '9', 'а'), 2, 8000)) 
+    + ' ' 
+    + UPPER(LEFT(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(p.firstName, '5', 'о'), '6', 'г'), '7', 'в'), '8', 'б'), '9', 'а'), 1)) 
+    + LOWER(SUBSTRING(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(p.firstName, '5', 'о'), '6', 'г'), '7', 'в'), '8', 'б'), '9', 'а'), 2, 8000)) 
+    + ' ' 
+    + UPPER(LEFT(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(p.patronymic, '5', 'о'), '6', 'г'), '7', 'в'), '8', 'б'), '9', 'а'), 1)) 
+    + LOWER(SUBSTRING(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(p.patronymic, '5', 'о'), '6', 'г'), '7', 'в'), '8', 'б'), '9', 'а'), 2, 8000)) AS fio,
+	CONVERT(varchar, p.birthdate, 104) AS birthDate,
+	p.snils,
+    c.nrPmuDepartName,
+    c.fedPosition,
+	c.fedPositionId,
+    CONVERT(VARCHAR, c.beginDate, 104) AS beginDate,
+    CONVERT(VARCHAR, c.endDate, 104) AS endDate,
+    STUFF((
+        SELECT DISTINCT ', ' + cert.specName 
+        FROM FRMR2.dbo.Certs cert 
+        WHERE cert.mrOid = c.mrOid 
+        FOR XML PATH('')
+    ), 1, 2, '') AS specNames,
+    c.nrPmuDepartId,
+    nsi.Id
+FROM FRMR2.dbo.Cards c
+INNER JOIN FRMR2.dbo.Persons p ON p.oid = c.mrOid
+LEFT JOIN NsiBase.Regiz.MedOrganizations nsi ON c.nrPmuDepartId = nsi.DepartOid
+WHERE c.oid = '__OID__'
+    AND (c.endDate IS NULL OR c.endDate > GETDATE())
+ORDER BY c.nrPmuDepartName, c.fedPosition;
